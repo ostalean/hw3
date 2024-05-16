b40b61afdf8e9f39831af96f089be2fab59cb556
ostalean ostale <antonino.ostale@kellogg.northwestern.edu>
2024-05-16 19:27:21 +0000

    advance 3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,4 +6,32 @@ class PlacesController < ApplicationController
     # render places/index view
   end
 
+  def show
+# find a Company
+params["id"]
+@places = Place.find_by({"id" => params["id"]})
+# render places/show view with details about Company
+
+
+  end
+
+  def new
+
+  end
+
+  def create 
+#   # start with a new Place
+@places = Place.new
+#   # assign user-entered form data to Place's columns
+@places["name"] = params["name"]
+
+#   # save Place row
+@places.save
+#   # redirect user
+  redirect_to "/places"
+
+
+  end
+
+
 end
