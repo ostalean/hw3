95b680b4a3feecfe65df830f460ef601d78aef89
ostalean ostale <antonino.ostale@kellogg.northwestern.edu>
2024-05-16 18:51:17 +0000

    advance 2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,26 +1,9 @@
 class PlacesController < ApplicationController
 
   def index
-    bitcoin = params["amount"]
-
-    # connect to the Coindesk API
-    # DON'T CHANGE THIS CODE
-    # ----------------------
-    require "net/http"
-    require "json"
-    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    bitcoin_data = JSON.parse(response)
-    # ----------------------
-  
-    # using Coindesk API, extract current rate of bitcoin in USD
-    @usd_rate = bitcoin_data["bpi"]["USD"]["rate_float"]
-  
-    # calculate value in USD of user's bitcoin
-    @usd_value = @usd_rate * bitcoin.to_f
-
-    # render bitcoins/index view
+    # find all Places rows
+    @places = Place.all
+    # render places/index view
   end
-  
+
 end
